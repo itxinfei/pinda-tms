1d6797fe62fb5fe6b1a4c5fa60494e0499120f15
pangzhao <747011882@qq.com>
2021-07-27 14:35:54 +0800

    补充文件

--- pd-base/src/main/java/com/itheima/pinda/controller/agency/FleetController.java
+++ pd-base/src/main/java/com/itheima/pinda/controller/agency/FleetController.java
@@ -27,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("sys/agency/fleet")
 public class FleetController {
+
     @Autowired
     private IPdFleetService fleetService;
 
